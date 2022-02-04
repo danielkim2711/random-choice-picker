58776aed24cb05bb3e97760d098c6acee32a6f91
Daniel Kim <danielkim2711@gmail.com>
2022-02-04 16:37:58 +1300

    Select random tag

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const textarea = document.querySelector('textarea');
 
 textarea.focus();
 
-textarea.addEventListener('keyup', (e) => createTags(e.target.value));
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => (e.target.value = ''), 10);
+    selectRandom();
+  }
+});
 
 function createTags(value) {
   const tags = value
@@ -19,4 +26,39 @@ function createTags(value) {
     tagEl.textContent = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function selectRandom() {
+  const times = 30;
+
+  // Set an interval
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+
+    setTimeout(() => unHighlightTag(randomTag), 100);
+  }, 100);
+
+  // Stop the interval
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    });
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
